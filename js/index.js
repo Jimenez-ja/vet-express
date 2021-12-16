2dfdd4301ebb9ba5a16376c72e87aabdbea0d58a
Jose Armando Jimenez Morales <jimenezz.jose7@gmail.com>
2021-12-15 22:25:52 -0600

    front v1.5

--- js/index.js
+++ js/index.js
@@ -4,3 +4,32 @@ const logOut = () => {
     window.location = './';
 
 }
+
+
+const loadServices = async() => {
+    let fetchData = {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }
+
+
+    await fetch('https://empresaurios.herokuapp.com/api/service', fetchData)
+        .then((res) => res.json())
+        .then((data) => {
+
+            if (data.service.length != 0) {
+
+                data.service.forEach(element => {
+                    $('#fillServices').append(`
+                    <li><a href="./detail-page.html?id=${element._id}">${element.name}</a></li>
+                    `);
+                });
+            }
+        })
+
+}
+
+
+loadServices();
